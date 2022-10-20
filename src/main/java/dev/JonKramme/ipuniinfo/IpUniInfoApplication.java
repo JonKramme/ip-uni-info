25a39fa65d3f6db3e6a0102193051bbe8232343b
JonKramme <86349628+JonKramme@users.noreply.github.com>
2022-10-20 20:23:14 +0200

    Initial commit

---
+++ src/main/java/dev/JonKramme/ipuniinfo/IpUniInfoApplication.java
@@ -0,0 +1,13 @@
+package dev.JonKramme.ipuniinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpUniInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpUniInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/JonKramme/ipuniinfo/IpUniInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.JonKramme.ipuniinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpUniInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
