aa7fee966ecd99584e148c7a81ccf3bf564a8259
JonKramme <86349628+JonKramme@users.noreply.github.com>
2022-10-20 21:41:52 +0200

    API Call Test

--- src/main/java/dev/JonKramme/ipuniinfo/controller/IpInfoController.java
+++ src/main/java/dev/JonKramme/ipuniinfo/controller/IpInfoController.java
@@ -3,6 +3,7 @@ package dev.JonKramme.ipuniinfo.controller;
 import dev.JonKramme.ipuniinfo.model.IpInfo;
 import dev.JonKramme.ipuniinfo.repository.IpInfoRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/ipcheck")
@@ -10,6 +11,11 @@ public class IpInfoController {
 
     @GetMapping("/{Ipv4Address}")
     public String requestIpInfo(@PathVariable String Ipv4Address,IpInfoRepository repo){
-        return Ipv4Address;
+        //Testing API Call (assuming ip is valid)
+        String url = "https://ipinfo.io/"+Ipv4Address+"/geo";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url,String.class);
+
+        return result;
     }
 }
