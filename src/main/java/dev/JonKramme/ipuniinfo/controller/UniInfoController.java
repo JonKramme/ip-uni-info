fb17139701eccabf807ca3cda618b0473c365b31
JonKramme <86349628+JonKramme@users.noreply.github.com>
2022-10-24 00:40:49 +0200

    Cleanup

--- src/main/java/dev/JonKramme/ipuniinfo/controller/UniInfoController.java
+++ src/main/java/dev/JonKramme/ipuniinfo/controller/UniInfoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/unicheck")
 public class UniInfoController {
@@ -33,7 +36,6 @@ public class UniInfoController {
         } catch (Exception e) {
             return new ResponseEntity<>(headers, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
         return new ResponseEntity<>(uniInfoEntity, headers, HttpStatus.OK);
 
     }
